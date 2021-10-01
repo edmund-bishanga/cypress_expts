3f39a40a38a1d008170f5f7b18aa96de50d6be8b
Edmund M BISHANGA <me.bishanga03@cantab.net>
2021-10-01 13:31:36 +0100

    cypress_expts: customisation of env/config variables, debug printing

--- cypress/integration/1-getting-started/test_strava_dashboard_cypress.js
+++ cypress/integration/1-getting-started/test_strava_dashboard_cypress.js
@@ -3,7 +3,8 @@
 // Purpose of this Test: 
 // + Get to the Dashboard Page
 // + Test 3 features: that ought to be essential/primary
-
+const CUSTOM_PAGELOAD_TIMEOUT = 120000 // ms
+const CUSTOM_EXEC_TIMEOUT = 80000 // ms
 const urlUnderTest = 'http://strava.com'
 const dashRoute = '/dashboard'
 const validUser = {
@@ -12,7 +13,13 @@ const validUser = {
 }
 
 describe(`Testing: ${urlUnderTest}: Login and Test userDashboard`, () => {
+    // eslint-disable cypress/no-unnecessary-waiting  // eslint_example
+    Cypress.config('pageLoadTimeout', CUSTOM_PAGELOAD_TIMEOUT)
+    expect(Cypress.config('pageLoadTimeout')).to.eq(CUSTOM_PAGELOAD_TIMEOUT)
+    Cypress.config('execTimeout', CUSTOM_EXEC_TIMEOUT)
     before(() => {
+      cy.log(`CypressConfig: ${JSON.stringify(Cypress.config(), null, 3)}`)
+      // console.log(`CypressConfig: ${JSON.stringify(Cypress.config(), null, 3)}`)
       cy.visit(urlUnderTest)
         .log('Get to Login Page')
         .get('.btn-login').should('contain', 'Log In').click()
