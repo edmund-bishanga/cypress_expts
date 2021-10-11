f61e98804aa6a1733af7e47b6a4f4c4563d4e0fb
Edmund M BISHANGA <me.bishanga03@cantab.net>
2021-10-11 12:10:23 +0100

    Strava Dashboard Testing: + editable timeouts + url verification Tested: locally: OK

--- cypress/integration/1-getting-started/test_strava_dashboard_cypress.js
+++ cypress/integration/1-getting-started/test_strava_dashboard_cypress.js
@@ -5,7 +5,15 @@
 // + Test 3 features: that ought to be essential/primary
 const CUSTOM_PAGELOAD_TIMEOUT = 120000 // ms
 const CUSTOM_EXEC_TIMEOUT = 80000 // ms
-const urlUnderTest = 'http://strava.com'
+const CUSTOM_CMD_TIMEOUT = 60000 // ms
+
+// eslint-disable cypress/no-unnecessary-waiting  // eslint_example
+Cypress.config('pageLoadTimeout', CUSTOM_PAGELOAD_TIMEOUT)
+expect(Cypress.config('pageLoadTimeout')).to.eq(CUSTOM_PAGELOAD_TIMEOUT)
+Cypress.config('execTimeout', CUSTOM_EXEC_TIMEOUT)
+Cypress.config('defaultCommandTimeout', CUSTOM_CMD_TIMEOUT)
+
+const urlUnderTest = 'https://www.strava.com'
 const dashRoute = '/dashboard'
 const validUser = {
     'email': Cypress.env('strava_username'),
@@ -13,12 +21,8 @@ const validUser = {
 }
 
 describe(`Testing: ${urlUnderTest}: Login and Test userDashboard`, () => {
-    // eslint-disable cypress/no-unnecessary-waiting  // eslint_example
-    Cypress.config('pageLoadTimeout', CUSTOM_PAGELOAD_TIMEOUT)
-    expect(Cypress.config('pageLoadTimeout')).to.eq(CUSTOM_PAGELOAD_TIMEOUT)
-    Cypress.config('execTimeout', CUSTOM_EXEC_TIMEOUT)
     before(() => {
-      cy.log(`CypressConfig: ${JSON.stringify(Cypress.config(), null, 3)}`)
+      cy.log(`DEBUG: CypressConfig: ${JSON.stringify(Cypress.config(), null, 3)}`)
       // console.log(`CypressConfig: ${JSON.stringify(Cypress.config(), null, 3)}`)
       cy.visit(urlUnderTest)
         .log('Get to Login Page')
@@ -30,8 +34,25 @@ describe(`Testing: ${urlUnderTest}: Login and Test userDashboard`, () => {
     })
     it('Validate Dashboard Page', () => {
         cy.location('pathname').should('include', dashRoute)
+        const locationVerStrs = [urlUnderTest, dashRoute]
+        locationVerStrs.forEach(str => {
+            cy.url().should('include', str)
+        })
     })
-    it('Test Dashboard Feature1: ABC', () => {
-        cy.log('Testing dashFeature1')
+    it('DashboardVerification: Primary Feature OK?', () => {
+        cy.log('Verify: 1: Default Feed: OK?')
+          .get('#feed-filter-btn').should('contain', 'Following')  // expDefault
+    })
+    it('DashboardVerification: Athlete Profile Summary, Present?', () => {
+        cy.log('Verify: 2: Athlete Profile Summary: OK?')
+        // Athlete's summary sidebar present, has avatar image, with src attribute
+    })
+    it('DashboardVerification: Can Return to HomePage?', () => {
+        cy.log('Verify: 3: Can Return to HomePage?')
+        //   .get('.nav-bar').should('include', 'Return to the Strava home page')
+    })
+    it('DashboardVerification: Able to SignOut?', () => {
+        cy.log('Verify: 4: Able to SignOut?')
+        //   .get('.user-menu').should('include.any.keys', 'avatar-athlete')
     })
 })
